9ab3de81adad6e5223de86f6d7da1f5549c91e7b
tylerharter <tylerharter@gmail.com>
2023-03-18 21:14:00 -0500

    US federal holidays

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# us-federal-holidays
-CSV containing holidays scraped from https://www.opm.gov/policy-data-oversight/pay-leave/federal-holidays
+# US Federal Holidays
+
+CSV containing holidays scraped from https://www.opm.gov/policy-data-oversight/pay-leave/federal-holidays by scrape.py.  Results checkpointed to history.csv.
+
+Notes:
+* the scraped data is split on `<section class="tab-content" title="Historical Data">`, and only the dates after are considered.  Removing this would scrape future as well as historical dates.
+* quote types are manually replaced
+* cells containing dates with years are ignored.  For example, 2011 contains an entry "Friday, December 31, 2010" -- this is not included in the CSV.
---
+++ scrape.py
@@ -0,0 +1,31 @@
+import requests, re, time
+import pandas as pd
+
+r = requests.get("https://www.opm.gov/policy-data-oversight/pay-leave/federal-holidays/#url=Historical-Data")
+r.raise_for_status()
+
+html = r.text.split('<section class="tab-content" title="Historical Data">')[1]
+
+rows = []
+for table in re.findall(r'<table class="DataTable HolidayTable">([\s\S]*?)</table>', html):
+    year = re.findall(r"(\d{4}) Holiday Schedule", table)
+    if len(year) == 1:
+        year = year[0]
+    else:
+        continue
+
+    for tr in re.findall(r"<tr>([\s\S]*?)</tr>", table):
+        row = []
+        for td in re.findall(r"<td>([\s\S]*?)</td>", tr):
+            row.append(re.sub(r"<.*>", "", td).replace("’", "'").strip())
+        if len(row) == 2:
+            years = re.findall("\d{4}", row[0])
+            if years and years[0] != year:
+                continue # this table specified for a different year
+            date = row[0].split(", ")[1] + ", " + year
+            date = time.strptime(date, "%B %d, %Y")
+            row[0] = time.strftime("%Y-%m-%d", date)
+            rows.append(row)
+            
+df = pd.DataFrame(rows, columns=["date", "holiday"])
+df.sort_values(by="date").to_csv("holidays.csv", index=False)
